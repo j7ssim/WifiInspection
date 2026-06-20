0d14c016dc0cfb95e460a2a25d6f65bc59917047
j7ssim <nnvjokbcttt0@gmail.com>
2026-06-20 16:32:56 +0300

    Update build.gradle.kts

--- WifiInspection/app/build.gradle.kts
+++ WifiInspection/app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
 }
 
 android {
@@ -14,19 +13,8 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
-
-    buildTypes {
-        debug {
-            isDebuggable = true
-        }
-
-        release {
-            isMinifyEnabled = false
-        }
-    }
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
 }
